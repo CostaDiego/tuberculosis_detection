042db6b17777e9b35c4e02a87029ce9aaa0c9c12
Diego Costa <diego.szct@gmail.com>
2022-10-02 01:50:15 -0300

    Chore(utils): Change to relative imports

--- utils/__init__.py
+++ utils/__init__.py
@@ -8,5 +8,5 @@ root = path.abspath("..")
 if root not in sys.path:
     sys.path.append(root)
 
-from utils.utils import load_from_yaml, generate_dataframe, load_dataframe
-from utils.errors import DatasetDescriptionError, DataframeGenerationError
+from .utils import load_from_yaml, generate_dataframe, load_dataframe
+from .errors import DatasetDescriptionError, DataframeGenerationError
