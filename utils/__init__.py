6fc81dd24d66f4cada569393f1d3617bddcf832a
Diego Costa <diego.szct@gmail.com>
2022-10-02 02:13:37 -0300

    Chore(utils): Rename file to dataframe to match its functionality

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ["utils"]
+__all__ = ["dataframe"]
 
 from os import path
 import sys
@@ -8,5 +8,5 @@ root = path.abspath("..")
 if root not in sys.path:
     sys.path.append(root)
 
-from .utils import load_from_yaml, generate_dataframe, load_dataframe
+from .dataframe import load_from_yaml, generate_dataframe, load_dataframe
 from .errors import DatasetDescriptionError, DataframeGenerationError
